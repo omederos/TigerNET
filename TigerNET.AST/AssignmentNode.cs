06586e8552dc4614a7ea9d2faf19c095eb79ba91
Oscar Mederos <omederos@gmail.com>
2012-02-28 03:58:13 -0500

    Chequeo semantico del AssignmentNode + Pruebas

--- TigerNET.AST/AssignmentNode.cs
+++ TigerNET.AST/AssignmentNode.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using TigerNET.Common;
 using TigerNET.Common.Errors;
+using TigerNET.Common.Types;
 
 namespace TigerNET.AST
 {
@@ -26,7 +27,37 @@ namespace TigerNET.AST
         }
 
         public override void CheckSemantic(Scope scope, IList<Error> errors) {
-            throw new NotImplementedException();
+            int errorsCount = errors.Count;
+            //Comprobamos que este correctamente Left
+            Left.CheckSemantic(scope, errors);
+            //Comprobamos que este correctamente Body
+            Body.CheckSemantic(scope, errors);
+
+            //Si ocurrio algun error, paramos
+            if (errorsCount != errors.Count) {
+                return;
+            }
+
+            //Comprobamos que ambas expresiones retornen valor...
+            ErrorsHelper.CheckIfReturnsValue(Left, errors,
+                                             "The expression that is being assigned a value must return a value");
+            ErrorsHelper.CheckIfReturnsValue(Body, errors,
+                                 "The expression to be assigned must return a value");
+            if (errorsCount != errors.Count) {
+                return;
+            }
+            
+            //Si el tipo de retorno es diferente
+            //TODO: Aceptar nil si es array, record o string!
+            if (Body.ReturnType != Left.ReturnType) {
+                //Si se esta asignando nil, comprobar que el tipo lo permita
+                if (!(Body.ReturnType is NilType) || !NilType.CanBeAssignedTo(Left.ReturnType))
+                {
+                    errors.Add(new UnexpectedTypeError(Line, Column, Body.ReturnType, Left.ReturnType));
+                }
+            }
+
+            //Esta expresion no retorna valor
         }
     }
 }
---
+++ TigerNET.Tests/Semantic/Assignment.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using TigerNET.AST;
+using TigerNET.Common.Errors;
+using TigerNET.Common.Types;
+
+namespace TigerNET.Tests.Semantic
+{
+    public class Assignment : SemanticTest
+    {
+        [Test]
+        public void Alias_String_OK()
+        {
+            var ast = (LetInEndNode)Utils.BuildAST(@"
+let 
+    type a = string 
+    var x : a := ""Hello""
+in 
+    x := ""Oscar""
+end");
+            ast.CheckSemantic(Scope, Errors);
+            Assert.That(Errors.Count == 0);
+            Assert.That(ast.ReturnType == null);
+        }
+
+        [Test]
+        public void Assign_To_Diferente_Record()
+        {
+            var ast = (LetInEndNode)Utils.BuildAST(@"
+let 
+    type r1 = {x : string } 
+    type r2 = {x : string } 
+    var x : r1 := r1{x = ""Hello""}
+in 
+    x := r2{x = ""Hello""}
+end");
+            ast.CheckSemantic(Scope, Errors);
+            Assert.That(Errors.Count == 1);
+            Assert.That(ast.ReturnType == null);
+        }
+
+
+        [Test]
+        public void Assign_Nil_To_Record()
+        {
+            var ast = (LetInEndNode)Utils.BuildAST(@"
+let 
+    type r1 = {x : string } 
+    var x := r1{x = """"}
+in 
+    x := nil
+end");
+            ast.CheckSemantic(Scope, Errors);
+            Assert.That(Errors.Count == 0);
+            Assert.That(ast.ReturnType == null);
+        }
+        [Test]
+        public void Assign_Nil_To_String()
+        {
+            var ast = (LetInEndNode)Utils.BuildAST(@"
+let  
+    var x := ""string""
+in 
+    x := nil
+end");
+            ast.CheckSemantic(Scope, Errors);
+            Assert.That(Errors.Count == 0);
+            Assert.That(ast.ReturnType == null);
+        }
+        [Test]
+        public void Assign_Nil_To_Array()
+        {
+            var ast = (LetInEndNode)Utils.BuildAST(@"
+let 
+    type r1 = array of int
+    var x := r1[0] of 0
+in 
+    x := nil
+end");
+            ast.CheckSemantic(Scope, Errors);
+            Assert.That(Errors.Count == 0);
+            Assert.That(ast.ReturnType == null);
+        }
+    }
+}
