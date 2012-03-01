d805f65ed4f09b9a5015c603359c0c4b7fe7dc9f
Oscar Mederos <omederos@gmail.com>
2012-03-01 03:46:39 -0500

    Chequeo semantico del ForToDo + Pruebas unitarias

--- TigerNET.AST/ForToDoNode.cs
+++ TigerNET.AST/ForToDoNode.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using TigerNET.Common;
 using TigerNET.Common.Errors;
+using TigerNET.Common.Types;
 
 namespace TigerNET.AST {
     public class ForToDoNode : ControlNode
@@ -23,9 +24,33 @@ namespace TigerNET.AST {
             throw new NotImplementedException();
         }
 
-        public override void CheckSemantic(Scope scope, IList<Error> errors)
-        {
-            throw new NotImplementedException();
+        public override void CheckSemantic(Scope scope, IList<Error> errors) {
+            //El 'for' define un nuevo scope
+            var scopeFor = new Scope(scope);
+            //Anadimos una variable de tipo entero de solo lectura al scope del 'for'
+            scopeFor.Add(VariableName, IntegerType.Create(), true);
+
+            //Evaluamos la expresion de inicio y la de fin.
+            //OJO: Con el Scope que veniamos usando hasta ahora, pues la variable del 'for' no es visible para estas expresiones
+            int errorsCount = errors.Count;
+            ExpressionInitial.CheckSemantic(scope, errors);
+            ExpressionFinal.CheckSemantic(scope, errors);
+            //Si ocurrio algun error, lo mas probable es que no se haya asignado un tipo de retorno a estas expresiones, por lo que terminamos
+            if (errorsCount != errors.Count) {
+                return;
+            }
+
+            //Comprobamos que retornen valores las expresiones anteriores y que sea entero
+            ErrorsHelper.CheckIfReturnTypeIsInt(ExpressionInitial, errors, "The expression that defines the first value in the 'for' must return a value", "The expression that defines the first value in the 'for' must return an integer");
+            ErrorsHelper.CheckIfReturnTypeIsInt(ExpressionFinal, errors, "The expression that defines the last value in the 'for' must return a value", "The expression that defines the last value in the 'for' must return an integer");
+
+            //Comprobamos el cuerpo
+            Body.CheckSemantic(scopeFor, errors);
+            
+            //Comprobamos que retorne algun valor
+            if (Body.ReturnsValue()) {
+                errors.Add(new MessageError(Line, Column, "Expression body of 'for' must not return a value"));
+            }
         }
     }
 }
---
+++ TigerNET.Tests/Semantic/ForToDo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using TigerNET.AST;
+using TigerNET.Common.Errors;
+using TigerNET.Common.Types;
+
+namespace TigerNET.Tests.Semantic
+{
+    public class ForToDo : SemanticTest
+    {
+        [Test]
+        public void For_OK()
+        {
+            var ast = Utils.BuildAST("for i := 1 to 10 do ()");
+            ast.CheckSemantic(Scope, Errors);
+            Assert.That(Errors.Count == 0);
+            Assert.That(!ast.ReturnsValue());
+        }
+        [Test]
+        public void For_IncorrectInitial()
+        {
+            var ast = Utils.BuildAST(@"for i := ""hello"" to 10 do ()");
+            ast.CheckSemantic(Scope, Errors);
+            Assert.That(Errors.Count == 1);
+            Assert.That(!ast.ReturnsValue());
+        }
+        [Test]
+        public void For_IncorrectFinal()
+        {
+            var ast = Utils.BuildAST(@"for i := 0 to ""hello"" do ()");
+            ast.CheckSemantic(Scope, Errors);
+            Assert.That(Errors.Count == 1);
+            Assert.That(!ast.ReturnsValue());
+        }
+        
+        [Test]
+        public void For_BodyReturnsValue()
+        {
+            var ast = Utils.BuildAST(@"for i := -10 to 10 do 1");
+            ast.CheckSemantic(Scope, Errors);
+            Assert.That(Errors.Count == 1);
+            Assert.That(!ast.ReturnsValue());
+        }
+
+        [Test]
+        public void AssignValueToForVariable()
+        {
+            var ast = Utils.BuildAST(@"for i := -10 to 10 do i := 0");
+            ast.CheckSemantic(Scope, Errors);
+            Assert.That(Errors.Count == 1);
+            Assert.That(!ast.ReturnsValue());
+        }
+    }
+}
